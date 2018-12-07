50eaeac966339772c71ade636d93abf151b7830d
dinsaren <dinsarenkh@gmail.com>
2018-12-07 23:44:16 +0700

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Dinsaren on 12/7/18.
+//  Copyright © 2018 Dinsaren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
