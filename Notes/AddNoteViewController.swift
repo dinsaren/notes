c1a8591a54aff5b110105cb72eb248a59bf6253f
dinsaren <dinsarenkh@gmail.com>
2018-12-11 16:54:45 +0700

    first commit

---
+++ Notes/AddNoteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddNoteViewController.swift
+//  Notes
+//
+//  Created by Dinsaren on 12/8/18.
+//  Copyright © 2018 Dinsaren. All rights reserved.
+//
+
+import UIKit
+
+class AddNoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Notes/CustomeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomeCollectionViewCell.swift
+//  Notes
+//
+//  Created by Dinsaren on 12/11/18.
+//  Copyright © 2018 Dinsaren. All rights reserved.
+//
+
+import UIKit
+
+class CustomeCollectionViewCell: UICollectionViewCell {
+    
+}
